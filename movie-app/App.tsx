136c7ccab86dd3ad0020130ca954baff198a0b4f
nurulaamalia <naamalia23@gmail.com>
2024-07-06 03:28:34 +0700

    resolve conflict

--- movie-app/App.tsx
+++ movie-app/App.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import BottomTabNavigator from './src/navigations/BottomTabNavigation'
 
-
 export default function App(): JSX.Element {
   return (
     <NavigationContainer>
