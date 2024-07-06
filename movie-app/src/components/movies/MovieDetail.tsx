4411048a19d589103c81a41c5cb7e71c47cd16bd
nurulaamalia <naamalia23@gmail.com>
2024-07-06 11:06:35 +0700

    develop home screen layout

---
+++ movie-app/src/components/movies/MovieDetail.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { API_URL, API_ACCESS_TOKEN } from '@env';
+
+const MovieDetail = ({ navigation }: any): any => {
+  const fetchData = (): void => {
+    console.log('API_URL:', API_URL);
+    console.log('API_ACCESS_TOKEN:', API_ACCESS_TOKEN);
+
+    if (API_URL == null || API_ACCESS_TOKEN == null) {
+      throw new Error('ENV not found');
+    }
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+      },
+    };
+
+    fetch(API_URL, options)
+      .then(async (response) => await response.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Movie Detail Page</Text>
+      <Button
+        title="Fetch Data"
+        onPress={() => {
+          fetchData();
+        }}
+      />
+    </View>
+  );
+};
+
+export default MovieDetail;
